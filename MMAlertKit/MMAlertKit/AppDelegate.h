690bc5e89518fbbea76e1dd551ff49f5dab4b7a3
yangshangbin <1058070112@qq.com>
2018-09-26 21:12:31 +0800

    add oc

---
+++ MMAlertKit/MMAlertKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MMAlertKit
+//
+//  Created by yangmoumou on 2018/9/26.
+//  Copyright © 2018年 yangmoumou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MMAlertKit/MMAlertKit/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  MMAlertKit
+//
+//  Created by yangmoumou on 2018/9/26.
+//  Copyright © 2018年 yangmoumou. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
